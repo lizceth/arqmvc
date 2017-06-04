373aa4dd96e8cfb4fa0bc11db1bb62e2001fedd7
lizceth <shanny.liz13@gmail.com>
2017-06-04 18:17:33 -0500

    modelo clases

--- app/models.py
+++ app/models.py
@@ -1,5 +1,32 @@
 from __future__ import unicode_literals
+from django.contrib.auth import User
 
 from django.db import models
 
-# Create your models here.
+class Cliente(models.Model):
+    nombres = models.CharField(max_length=50)
+    telefono = models.CharField(max_length=10)
+    def __str__(self):
+        return self.nombres
+
+class Pedido(models.Model):
+    """docstring for Pedido"""
+    descripcion = models.TextField()
+    fecha = models.DateField(auto_now_add=True)
+    cliente = models.ForeignKey(Cliente)
+
+    def __str__(self):
+        return self.cliente 
+
+class Atencion(models.Model):
+
+    """docstring for Atencion"""
+    fecha = models.DateField(auto_now_add=True)
+    Pedido = models.ForeignKey(Pedido)
+    doctor = models.ForeignKey(User)
+    tratamiento = models.TextField()
+
+    def __str__(self):
+        return "%s - %s " % (self.fecha, self.pedido.cliente)
+        
+
